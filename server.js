9201ae5f9fb5c2109e591794d9d4668697efcb40
SusanHokonya <129788374+SusanHokonya@users.noreply.github.com>
2023-04-05 09:27:32 +0200

    Add files via upload

---
+++ server.js
@@ -0,0 +1,45 @@
+//importing libraries that we installed using npm
+const express = require("express")
+const app = express()
+const bcrypt = require("bcrypt")   // importing bcrypt package
+const initializePassport = require("./passport-config")
+
+const users = []
+
+
+app.use(express.urlencoded({extended: false}))
+
+app.post("/register", async(req, res) => {
+    try {
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: req.body.password,
+        })
+
+        console.log(users);
+        res.redirect("/login")
+
+    } catch (e) {
+        console.log(e);
+        res.redirect("/register")
+    }
+})
+
+// routes
+app.get('/', (req, res)=> {
+    res.render("./index.ejs")
+})
+app.get('/login',(req, res) => {
+    res.render("login.ejs")
+})
+app.get('/register', (req, res)=>{
+    res.render("register.ejs")
+})
+//End routes
+
+
+
+app.listen(3000)
